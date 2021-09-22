60f397206e335e1d5bd2422d659cbf2799fe9c67
Cameron Mills <camhmills@gmail.com>
2021-09-22 17:28:04 -0400

    working home and registration page

--- public/home.js
+++ public/home.js
@@ -1,3 +1,4 @@
+jobsContainer = document.querySelector(".jobs")
 const viewJobs = async () => {
     const url = "http://localhost:3008/user/jobs/welds";
 
@@ -8,9 +9,26 @@ const viewJobs = async () => {
             "Content-type": "application/json"
         },
     })
-    const jobJson = jobData.json();
+    const jobJson = await jobData.json();
 
     console.log(jobJson)
-}
+
+    for (const job of jobJson[0]) {
+    const accordion = document.createElement("div");
+    const accordionButton = document.createElement("button");
+    accordionButton.className = "collapsible"
+    accordionButton.innerHTML = `${job.name} ${job.job_number}`
+    accordion.append(accordionButton)
+    for (const weld of jobJson[1]) {
+        const welds = document.createElement("p");
+        if (weld.jobId === job.id) {
+            welds.innerHTML = `${weld.partNumber1} & ${weld.partNumber2} w/ code: ${weld.weldId}`
+        }
+        accordion.append(welds)
+    }
+    
+    jobsContainer.append(accordion)
+    };
+};
 
 viewJobs();
